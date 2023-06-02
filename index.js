87bdf0f804df9e2377997cb28ebd95f8fe9dfc2c
Premananda Mohapatra <prem.mohapatra@gmail.com>
2023-06-02 11:46:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 4000;
 
 app.get("/", (req, res) => {
+	console.log("I have been pinged");
 	res.json({ status: 'Ok' });
 });
 /*
 /deeplink?pa=&pn=&tn=&am=
 */
 app.get("/deeplink", (req, res) => {
+	console.log("Incoming request for payment");
 	let { pa, pn, tn, am } = req.query;
   	//Construct like this "upi://pay?pa=prem.mohapatra@oksbi&pn=PNLBot&am=1000&tn=MonthlyFees&tr=uuid";
 	let url = new URL("upi://pay");
